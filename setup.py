36f207e8ce412ef86de93a9392c6413e4507de1e
Nidhish Wakodikar <nidhishwakodikar@gmail.com>
2024-02-14 14:31:55 +0530

    author commit

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author='Krish',
+    author='Nidhish',
     author_email='nidhishwakodikar@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirement.txt')
